2b46501be31499ef3898392fce3deaf0c18d4579
BaloeTheBear <76252234+BaloeTheBear@users.noreply.github.com>
2025-08-09 16:19:20 +0200

    Step 10: Add name route (query params)

--- myApp.js
+++ myApp.js
@@ -40,4 +40,10 @@ app.get("/:word/echo", function (req, res) {
   });
 });
 
+app.route("/name").get(function (req, res) {
+  res.json({
+    name: req.query.firstname + " " + req.query.lastname,
+  });
+});
+
 module.exports = app;
